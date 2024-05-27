e3b404955ee1394a9c52967e9d3b68c6968769e0
Nagib <131501717+NicolasNagib653@users.noreply.github.com>
2024-05-27 10:23:44 -0300

    Atualizar o script.js

--- jogos/jogo_da_velha/script.js
+++ jogos/jogo_da_velha/script.js
@@ -47,6 +47,21 @@ function vitoria() {
     let vitoria = document.querySelector("#vitoria")
     let visor = document.querySelector("#visor")
 
+if((tabuleiro[0] == "o" && tabuleiro[3] == "o" && tabuleiro[6] == "o")||
+       (tabuleiro[1] == "o" && tabuleiro[4] == "o" && tabuleiro[7] == "o")||
+       (tabuleiro[2] == "o" && tabuleiro[5] == "o" && tabuleiro[8] == "o")||
+       (tabuleiro[0] == "o" && tabuleiro[1] == "o" && tabuleiro[2] == "o")||
+       (tabuleiro[3] == "o" && tabuleiro[4] == "o" && tabuleiro[5] == "o")||
+       (tabuleiro[6] == "o" && tabuleiro[7] == "o" && tabuleiro[8] == "o")||
+       (tabuleiro[0] == "o" && tabuleiro[4] == "o" && tabuleiro[8] == "o")||
+       (tabuleiro[2] == "o" && tabuleiro[4] == "o" && tabuleiro[6] == "o")){
+        setTimeout(function () {
+            document.getElementById("tabuleiro").style.display = "none"
+            vitoria.style.display = "block"
+            visor.innerHTML = `O ${nome2.value} ganhou!`
+        }, 300)
+    }
+
     if ((tabuleiro[0] == "x" && tabuleiro[3] == "x" && tabuleiro[6] == "x")||
         (tabuleiro[1] == "x" && tabuleiro[4] == "x" && tabuleiro[7] == "x")||
         (tabuleiro[2] == "x" && tabuleiro[5] == "x" && tabuleiro[8] == "x")||
@@ -61,18 +76,5 @@ function vitoria() {
             visor.innerHTML = `O ${nome1.value} ganhou!`
         }, 300)
     }
-    if((tabuleiro[0] == "o" && tabuleiro[3] == "o" && tabuleiro[6] == "o")||
-       (tabuleiro[1] == "o" && tabuleiro[4] == "o" && tabuleiro[7] == "o")||
-       (tabuleiro[2] == "o" && tabuleiro[5] == "o" && tabuleiro[8] == "o")||
-       (tabuleiro[0] == "o" && tabuleiro[1] == "o" && tabuleiro[2] == "o")||
-       (tabuleiro[3] == "o" && tabuleiro[4] == "o" && tabuleiro[5] == "o")||
-       (tabuleiro[6] == "o" && tabuleiro[7] == "o" && tabuleiro[8] == "o")||
-       (tabuleiro[0] == "o" && tabuleiro[4] == "o" && tabuleiro[8] == "o")||
-       (tabuleiro[2] == "o" && tabuleiro[4] == "o" && tabuleiro[6] == "o")){
-        setTimeout(function () {
-            document.getElementById("tabuleiro").style.display = "none"
-            vitoria.style.display = "block"
-            visor.innerHTML = `O ${nome2.value} ganhou!`
-        }, 300)
-    }
+    
 }
